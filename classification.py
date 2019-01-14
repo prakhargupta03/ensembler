0d398e9bc04195f3c8c7318bedc0e6536bebc9a9
PrakharTU <prakharpg.83@gmail.com>
2019-01-14 22:29:32 +0530

    added test for wrong input file

--- classification.py
+++ classification.py
@@ -1,6 +1,6 @@
+flag = 1
 def mail(send_to_email):
 	import os
-	
 	print("\n\nMailing the results")
 	import smtplib
 	from email.mime.text import MIMEText
@@ -11,8 +11,9 @@ def mail(send_to_email):
 	email = os.environ.get('EMAIL')
 	password = os.environ.get('PASSWORD')
 	subject = "Ensembling Model Results"
-	message = "Please find the attachment named as result.txt for result"
-	file_location = 'result.txt'
+	message = "Please find the attachment named as result.txt and result.csv  for result"
+	
+
 	msg = MIMEMultipart()
 
 	msg['From'] = email
@@ -21,27 +22,38 @@ def mail(send_to_email):
 
 	body = message
 
-	msg.attach(MIMEText(body, 'plain'))
+	if(flag == 1):
 
-	filename = ntpath.basename(file_location)
-	attachment = open(file_location, "rb")
+		file_location = 'result_reg.txt'
+		filename = ntpath.basename(file_location)
+		attachment = open(file_location, "rb")
+		part = MIMEBase('application', 'octet-stream')
+		part.set_payload((attachment).read())
+		encoders.encode_base64(part)
+		part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+		msg.attach(part)
+		
+		file_location = 'result_reg.csv'
+		filename = ntpath.basename(file_location)
+		attachment = open(file_location, "rb")
+		part = MIMEBase('application', 'octet-stream')
+		part.set_payload((attachment).read())
+		encoders.encode_base64(part)
+		part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+		msg.attach(part)
 
-	part = MIMEBase('application', 'octet-stream')
-	part.set_payload((attachment).read())
-	encoders.encode_base64(part)
-	part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
-	msg.attach(part)
-
-	file_location = 'result.csv'
-	filename = ntpath.basename(file_location)
-	attachment = open(file_location, "rb")
-	part = MIMEBase('application', 'octet-stream')
-	part.set_payload((attachment).read())
-	encoders.encode_base64(part)
-	part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-	msg.attach(part)
+	else:
+		body = "Please find the attachment named as result.txt for result"
+		file_location = 'result_reg.txt'
+		filename = ntpath.basename(file_location)
+		attachment = open(file_location, "rb")
+		part = MIMEBase('application', 'octet-stream')
+		part.set_payload((attachment).read())
+		encoders.encode_base64(part)
+		part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+		msg.attach(part)
 	
+	msg.attach(MIMEText(body, 'plain'))
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.starttls()
 	server.login(email, password)
@@ -51,84 +63,91 @@ def mail(send_to_email):
 	print("\n\nResults Mailed")
 
 def calculate_results(input_file):
-	import pandas as pd
-	import random as r
-	dataset =  pd.read_csv(input_file)
-	list_col = dataset.columns
-	first_col = list_col[0]
-	list_col = list_col[1:]
-	dataset.rename(columns={first_col:'Actual'},inplace= True)
+	try:
+		import pandas as pd
+		import random as r
+		dataset =  pd.read_csv(input_file)
+		list_col = dataset.columns
+		first_col = list_col[0]
+		list_col = list_col[1:]
+		dataset.rename(columns={first_col:'Actual'},inplace= True)
 
-	l = dataset['Actual'].value_counts().to_dict()
-	val = [*l]
-	print(val)
-	#print(dataset.head())
-
-	
-	#print(list_col)
-
-	ll = 2
-	hl = len(list_col)
-	max_acc = 0.0
-	max_acc_model_count = 0
-	max_acc_model_names = []
-	df1 = pd.DataFrame(columns=["Iteration_No", "Model_Count","Models_Picked","Accuracy_Score"])
-
-
-	for k in range(100):
-		#print("Iteration",str(k+1))
-		i = r.randint(2,hl)
-		#print("\n\nNo of models picked:",i)
+		l = dataset['Actual'].value_counts().to_dict()
+		val = [*l]
+		print(val)
+		#print(dataset.head())
 
 		
-		models = r.sample(list(list_col),i)
-		#print("Models picked: ",models)
-		
-		df = pd.DataFrame(columns=["Actual","Predicted"])
-		df['Actual'] = dataset['Actual']
+		#print(list_col)
 
-		for j in range(len(dataset['Actual'])):
-			vote = {}
-			for c in val:
-				vote[c] = 0
-			for m in models:
-				vote[dataset.iloc[j][m]] = vote[dataset.iloc[j][m]] + 1
-			#print(vote)		
-			v=list(vote.values())
-			key=list(vote.keys())
-			df.loc[j,'Predicted'] = key[v.index(max(v))]
-			#print(df.loc[j,'Predicted'])
+		ll = 2
+		hl = len(list_col)
+		max_acc = 0.0
+		max_acc_model_count = 0
+		max_acc_model_names = []
+		df1 = pd.DataFrame(columns=["Iteration_No", "Model_Count","Models_Picked","Accuracy_Score"])
 
-		#print(df.head())
-		 
-		from sklearn.metrics import accuracy_score
-		Actual = df['Actual'].values
-		Pred = df['Predicted'].values
-		#print("Accuracy_score: ",round(accuracy_score(Actual,Pred),5),"\n\n")
-		df1.loc[k,"Iteration_No"] = k
-		df1.loc[k,"Model_Count"] = i
-		df1.at[k,"Models_Picked"] = models
-		df1.loc[k,"Accuracy_Score"] = round(accuracy_score(Actual,Pred),5)
-		if(round(accuracy_score(Actual,Pred),5) > max_acc):
-			max_acc = round(accuracy_score(Actual,Pred),5)
-			max_acc_model_count = i
-			max_acc_model_names = models
-		saved_file = "Predicted"+str(k)+".csv"
-		df.to_csv(saved_file,encoding='utf-8', index=False)
+
+		for k in range(100):
+			#print("Iteration",str(k+1))
+			i = r.randint(2,hl)
+			#print("\n\nNo of models picked:",i)
+
+			
+			models = r.sample(list(list_col),i)
+			#print("Models picked: ",models)
+			
+			df = pd.DataFrame(columns=["Actual","Predicted"])
+			df['Actual'] = dataset['Actual']
+
+			for j in range(len(dataset['Actual'])):
+				vote = {}
+				for c in val:
+					vote[c] = 0
+				for m in models:
+					vote[dataset.iloc[j][m]] = vote[dataset.iloc[j][m]] + 1
+				#print(vote)		
+				v=list(vote.values())
+				key=list(vote.keys())
+				df.loc[j,'Predicted'] = key[v.index(max(v))]
+				#print(df.loc[j,'Predicted'])
+
+			#print(df.head())
+			
+			from sklearn.metrics import accuracy_score
+			Actual = df['Actual'].values
+			Pred = df['Predicted'].values
+			#print("Accuracy_score: ",round(accuracy_score(Actual,Pred),5),"\n\n")
+			df1.loc[k,"Iteration_No"] = k
+			df1.loc[k,"Model_Count"] = i
+			df1.at[k,"Models_Picked"] = models
+			df1.loc[k,"Accuracy_Score"] = round(accuracy_score(Actual,Pred),5)
+			if(round(accuracy_score(Actual,Pred),5) > max_acc):
+				max_acc = round(accuracy_score(Actual,Pred),5)
+				max_acc_model_count = i
+				max_acc_model_names = models
+			saved_file = "Predicted"+str(k)+".csv"
+			df.to_csv(saved_file,encoding='utf-8', index=False)
+			
+		df1.to_csv("result.csv",encoding='utf-8', index=False)
 		
-	df1.to_csv("result.csv",encoding='utf-8', index=False)
-	
-	print("\n\nmax_Acc:",max_acc)
-	print("max_acc_model_count:",max_acc_model_count)
-	print("max_acc_model_names:",max_acc_model_names)
-	
-	
-	f = open("result.txt","w+")
-	f.write("Results are:\n\n")
-	f.write("Maximum Accuracy is: "+str(max_acc)+"\n")
-	f.write("No of models used: "+str(max_acc_model_count)+"\n")
-	f.write("Models used: "+str(max_acc_model_names)+"\n")
-	f.close()
+		print("\n\nmax_Acc:",max_acc)
+		print("max_acc_model_count:",max_acc_model_count)
+		print("max_acc_model_names:",max_acc_model_names)
+		
+		
+		f = open("result.txt","w+")
+		f.write("Results are:\n\n")
+		f.write("Maximum Accuracy is: "+str(max_acc)+"\n")
+		f.write("No of models used: "+str(max_acc_model_count)+"\n")
+		f.write("Models used: "+str(max_acc_model_names)+"\n")
+		f.close()
+	except:
+		global flag
+		flag = 0
+		f = open("result.txt","w+")
+		f.write("You have uploaded the file in correct format. Please check the file format as specified on our webpage.")
+		f.close()
 def main():
     # input_file = "classification.csv"
     # input_file = "Data_MultiClass.csv"
--- regression.py
+++ regression.py
@@ -1,3 +1,4 @@
+flag = 1
 def mail(send_to_email):
 	import os
 	print("\n\nMailing the results")
@@ -21,26 +22,38 @@ def mail(send_to_email):
 
 	body = message
 
+	if(flag == 1):
+
+		file_location = 'result_reg.txt'
+		filename = ntpath.basename(file_location)
+		attachment = open(file_location, "rb")
+		part = MIMEBase('application', 'octet-stream')
+		part.set_payload((attachment).read())
+		encoders.encode_base64(part)
+		part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+		msg.attach(part)
+		
+		file_location = 'result_reg.csv'
+		filename = ntpath.basename(file_location)
+		attachment = open(file_location, "rb")
+		part = MIMEBase('application', 'octet-stream')
+		part.set_payload((attachment).read())
+		encoders.encode_base64(part)
+		part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+		msg.attach(part)
+
+	else:
+		body = "Please find the attachment named as result.txt for result"
+		file_location = 'result_reg.txt'
+		filename = ntpath.basename(file_location)
+		attachment = open(file_location, "rb")
+		part = MIMEBase('application', 'octet-stream')
+		part.set_payload((attachment).read())
+		encoders.encode_base64(part)
+		part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+		msg.attach(part)
+	
 	msg.attach(MIMEText(body, 'plain'))
-	
-	file_location = 'result_reg.txt'
-	filename = ntpath.basename(file_location)
-	attachment = open(file_location, "rb")
-	part = MIMEBase('application', 'octet-stream')
-	part.set_payload((attachment).read())
-	encoders.encode_base64(part)
-	part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-	msg.attach(part)
-	
-	file_location = 'result_reg.csv'
-	filename = ntpath.basename(file_location)
-	attachment = open(file_location, "rb")
-	part = MIMEBase('application', 'octet-stream')
-	part.set_payload((attachment).read())
-	encoders.encode_base64(part)
-	part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-	msg.attach(part)
-	
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.starttls()
 	server.login(email, password)
@@ -50,73 +63,81 @@ def mail(send_to_email):
 	print("\n\nResults Mailed")
 	
 def calculate(filepath):
-	import pandas as pd
-	import random
-	import copy
+	try:
+		import pandas as pd
+		import random
+		import copy
 
-	dataset = pd.read_csv(filepath)
-	models_check = dataset.iloc[:,1:].values
-	actual = dataset.iloc[:,0].values
-	err = 0.1
-	number_of_models = len(models_check[1])
+		dataset = pd.read_csv(filepath)
+		models_check = dataset.iloc[:,1:].values
+		actual = dataset.iloc[:,0].values
+		err = 0.1
+		number_of_models = len(models_check[1])
 
-	accuracy_final = 0
-	max_acc_model_count = 0
-	max_acc_model_names = []
-	ind = 0
-	f1 = open("result_reg.csv","w+")
-	f1.write("Iteration,No_of_models_picked,Models_Picked,Accuracy\n")
-	
-	for i in range(100):
+		accuracy_final = 0
+		max_acc_model_count = 0
+		max_acc_model_names = []
+		ind = 0
+		f1 = open("result_reg.csv","w+")
+		f1.write("Iteration,No_of_models_picked,Models_Picked,Accuracy\n")
 		
-		num_of_picks = random.randint(1,number_of_models + 1)
-		r=[0]
-		res=[0]
-		mn=[0]
-		for j in range(0,num_of_picks):
-			model_number = random.randint(+0,number_of_models)
-			mn.insert(j,model_number)
-			r.insert(j,models_check[:,model_number-1])
-		
-		for j in range(0,len(r[0])):
-			sum_column = 0
-			for k in range(0,num_of_picks):
-				sum_column = sum_column + r[k][j]
-			res.insert(j,sum_column/num_of_picks)
+		for i in range(100):
 			
-		count=0
-		for j in range(0,len(actual)):
-			if(abs(actual[j]-res[j])/actual[j]<=err or abs(actual[j]-res[j])/res[j]<=err):
-				count = count+1
-		accuracy = (count*100)/len(actual)
-		
-		
-		l = []
-		for y in range(0,len(mn)-1):
-			x = "M"+ str(mn[y]+1)
-			l.append(x)
-		
-		f1.write(str(i)+","+str(num_of_picks)+","+"\""+str(l)+"\""+","+str(accuracy)+"\n")
-		
-		if(accuracy >= accuracy_final):
-			accuracy_final = accuracy
-			model_numbers_final = copy.deepcopy(mn)
-			res_final = copy.deepcopy(res)
-			max_acc_model_count = num_of_picks
-			max_acc_model_names = l
+			num_of_picks = random.randint(1,number_of_models + 1)
+			r=[0]
+			res=[0]
+			mn=[0]
+			for j in range(0,num_of_picks):
+				model_number = random.randint(+0,number_of_models)
+				mn.insert(j,model_number)
+				r.insert(j,models_check[:,model_number-1])
+			
+			for j in range(0,len(r[0])):
+				sum_column = 0
+				for k in range(0,num_of_picks):
+					sum_column = sum_column + r[k][j]
+				res.insert(j,sum_column/num_of_picks)
+				
+			count=0
+			for j in range(0,len(actual)):
+				if(abs(actual[j]-res[j])/actual[j]<=err or abs(actual[j]-res[j])/res[j]<=err):
+					count = count+1
+			accuracy = (count*100)/len(actual)
+			
+			
+			l = []
+			for y in range(0,len(mn)-1):
+				x = "M"+ str(mn[y]+1)
+				l.append(x)
+			
+			f1.write(str(i)+","+str(num_of_picks)+","+"\""+str(l)+"\""+","+str(accuracy)+"\n")
+			
+			if(accuracy >= accuracy_final):
+				accuracy_final = accuracy
+				model_numbers_final = copy.deepcopy(mn)
+				res_final = copy.deepcopy(res)
+				max_acc_model_count = num_of_picks
+				max_acc_model_names = l
 
 
-	print(accuracy_final)   
-	print(max_acc_model_count)   
-	print(max_acc_model_names)
-	
-	f = open("result_reg.txt","w+")
-	f.write("Results are:\n\n")
-	f.write("Maximum Accuracy is: "+str(accuracy_final)+"\n")
-	f.write("No of models used: "+str(max_acc_model_count)+"\n")
-	f.write("Models used: "+str(max_acc_model_names)+"\n")
-	f.close()
-	f1.close()
+		print(accuracy_final)   
+		print(max_acc_model_count)   
+		print(max_acc_model_names)
+		
+		f = open("result_reg.txt","w+")
+		f.write("Results are:\n\n")
+		f.write("Maximum Accuracy is: "+str(accuracy_final)+"\n")
+		f.write("No of models used: "+str(max_acc_model_count)+"\n")
+		f.write("Models used: "+str(max_acc_model_names)+"\n")
+		f.close()
+		f1.close()
+	except:
+		global flag
+		flag = 0
+		f = open("result_reg.txt","w+")
+		f.write("You have uploaded the file in correct format. Please check the file format as specified on our webpage.")
+		f.close()
+
 
 def main():
     # input_file = "data2.csv"
